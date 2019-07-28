7719f0ff3ee371f02ae0c0885fd1ad99b7c8a550
kevin552216 <kevin552216@gmail.com>
2019-07-28 23:05:23 +0800

    Add files via upload

--- psrezip.ps1
+++ psrezip.ps1
@@ -2,4 +2,4 @@
 $f = Read-Host -Prompt 'input folder name'
 New-Item -ItemType directory -Path ./$f'rezip'
 $folders=Get-ChildItem ./$f/* | Select-Object BaseName
-$folders | ForEach-Object {$b=$_.BaseName;& 'C:\Program Files\7-Zip\7z.exe' a -tzip ./$f'rezip'/$b.zip ./$f/$b/*}
+$folders | ForEach-Object {$b=$_.BaseName;& 'C:\Program Files\7-Zip\7z.exe' a -tzip ./$f'rezip'/$b.zip ./$f/$b/*.jpg ./$f/$b/*.png ./$f/$b/*.jpeg}
